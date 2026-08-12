66d7b049d32a9468d7098bbd375809cd24595790
rajasrimcse25-cmd <rajasrim.cse25@ifet.ac.in>
2026-08-12 11:06:37 +0530

    Add files via upload

---
+++ WrapperDemo.java
@@ -0,0 +1,14 @@
+public class WrapperDemo{
+  public static void main(String[] args){
+    int a=10;
+    float b=5.5f;
+    Integer i=a;
+    Float f=b;
+    int x=i;
+    float y=f;
+    System.out.println(i);
+    System.out.println(f);
+    System.out.println(x);
+    System.out.println(y);
+    }
+ }
